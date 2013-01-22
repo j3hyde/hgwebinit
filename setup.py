314b1d2a26e561d58827a88d5fa7ee646d4ef2a6
jkyllo <none@none>
2013-01-21 20:06:56 -0800

    Changed project URL and reduced version prior to release.

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='hgweb-init',
-      version='1.0dev',
+      version='0.1.0dev',
       description='Mercurial hgweb init support',
       long_description=read('README.md'),
       author='Jeffrey Kyllo',
       author_email='jkyllo@echospiral.com',
-      url='https://echospiral.com/trac/hgweb-init',
+      url='https://bitbucket.org/j3hyde/hgwebinit',
       packages=['hgwebinit'],
       package_dir={'hgwebinit': 'src'}
      )
